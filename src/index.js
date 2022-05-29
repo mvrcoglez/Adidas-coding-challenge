e1c0774c7303d419c11b111c7bb03815f1ce2be4
Marco Gonzalez <gonzalez.marco95@gmail.com>
2022-05-29 18:58:52 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <h1>Your Team Adidas</h1>
+  </StrictMode>
+);
